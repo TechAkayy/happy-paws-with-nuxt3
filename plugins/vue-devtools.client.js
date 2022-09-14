455fb79d2c9de8be2c328bcb1a5d239ba4078ab8
akkayy <techakayy@gmail.com>
2022-09-14 18:52:28 +1000

    3. Installed vue-devtools and added plugin to use it

---
+++ plugins/vue-devtools.client.js
@@ -0,0 +1,9 @@
+import devtools from '@vue/devtools'
+
+export default defineNuxtPlugin((nuxtApp) => {
+	if (process.env.NODE_ENV /* nuxtApp.isDev */ === 'development') {
+		devtools.connect(/* host, port */)
+		// Expose connect on window's object so that connection with Vue Devtools can be conditionally triggered from within Pinegrow
+		// window.devtools = devtools
+	}
+})
