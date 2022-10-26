a38a8ed247e0f6a46fe5439996841c3634d8be7b
Akayy <techakayy@gmail.com>
2022-10-26 23:03:56 +1100

    app tree showcase updates

--- components/AdoptSection.vue
+++ components/AdoptSection.vue
@@ -1,41 +1,39 @@
 <script setup>
-	import { useCounterStore } from '~~/store/counter'
-	import { storeToRefs } from 'pinia'
+import { useCounterStore } from '~~/store/counter'
+import { storeToRefs } from 'pinia'
 
-	const counterStore = useCounterStore()
-	const { name, doubleCount } = storeToRefs(counterStore)
+const counterStore = useCounterStore()
+const { name, doubleCount } = storeToRefs(counterStore)
 
-	const { data: pets } = await useAsyncData('pets', async () => {
-		return await usePets()
-	})
-
-	// const pets = await usePets()
+const { data: pets } = await useAsyncData('pets', async () => {
+  return await usePets()
+})
 </script>
 <template>
-    <section class="bg-primary-500 py-24 text-center text-gray-50">
-        <div class="container mx-auto px-4">
-            <div class="-mx-4 flex flex-wrap items-center justify-center mb-12">
-                <div class="px-4 text-center w-full md:w-10/12 xl:w-9/12">
-                    <h2 class="font-medium mb-2 uppercase">Adopt A Pet</h2>
-                    <h3 class="capitalize font-bold mb-4 text-4xl text-white">
-						Small gesture for a big impact </h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-            </div>
-            <div class="-mx-3 flex flex-wrap justify-center mb-12">
-                <BaseNavButton/>
-                <BaseNavButton/>
-                <AdoptSectionCard v-for="(pet, index) in pets" :breed="pet.Breed" :date-of-birth="pet.Date_Of_Birth" :id="pet.id" :image-url="pet.Image_URL" :name="pet.Name" :sex="pet.Sex" :species="pet.Species"/>
-            </div>
-            <div>
-                <NuxtLink to="/adopt" class="bg-secondary-500 hover:bg-secondary-600 inline-block px-6 py-2 rounded-full text-white">Meet Them All</NuxtLink>
-            </div>
+  <section class="bg-primary-500 py-24 text-center text-gray-50">
+    <div class="container mx-auto px-4">
+      <div class="-mx-4 flex flex-wrap items-center justify-center mb-12">
+        <div class="px-4 text-center w-full md:w-10/12 xl:w-9/12">
+          <h2 class="font-medium mb-2 uppercase">Adopt A Pet</h2>
+          <h3 class="capitalize font-bold mb-4 text-4xl text-white">
+            Small gesture for a big impact </h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
-    </section>
+      </div>
+      <div class="-mx-3 flex flex-wrap justify-center mb-12">
+        <AdoptSectionCard :pet="pet" v-for="(pet, index) in pets" />
+      </div>
+      <div>
+        <NuxtLink to="/adopt"
+          class="bg-secondary-500 hover:bg-secondary-600 inline-block px-6 py-2 rounded-full text-white">Meet Them All
+        </NuxtLink>
+      </div>
+    </div>
+  </section>
 </template>
 <style scoped>
-	.adopt-section {
-		color: green;
-		font-weight: bold;
-	}
+.adopt-section {
+  color: green;
+  font-weight: bold;
+}
 </style>
--- components/AdoptSectionCard.vue
+++ components/AdoptSectionCard.vue
@@ -1,54 +1,33 @@
 <script setup>
-	defineProps({
-		id: {
-			type: Number,
-			required: true
-		},
-		name: {
-			type: String,
-			required: true
-		},
-		species: {
-			type: String,
-			required: true
-		},
-		breed: {
-			type: String,
-			required: true
-		},
-		sex: {
-			type: String,
-			required: true
-		},
-		dateOfBirth: {
-			type: String,
-			required: false
-		},
-		imageUrl: {
-			type: String,
-			required: true
-		}
-	})
-    const petArrayss = ref(null)
+const props = defineProps({
+  pet: {
+    type: Object,
+    required: true
+  }
+})
+
+const { id: id, Name: name, Species: species, Breed: breed, Sex: sex, Date_Of_Birth: dateOfBirth, Image_URL: imageUrl } = toRefs(props.pet)
+
 </script>
 <template>
-    <div class="p-3 w-full md:w-6/12 lg:w-4/12 xl:w-3/12">
-        <div class="bg-white px-6 py-12 rounded-3xl text-center text-gray-400">
-            <img :src="imageUrl" class="mb-4 mx-auto rounded-xl w-full sm:w-9/12" alt="..." width="600" height="600"/>
-            <h4 class="font-bold mb-2 text-2xl text-gray-900">{{ name }}</h4>
-            <p class="mb-6 text-sm">
-				{{ breed }} </p>
-            <BaseNavButton :to="`/adopt/${id}`"><span>Adopt Now</span>
-                <template v-slot:append>
-                    <Icon name="ri:heart-add-fill" size="24" class="text-yellow-700"></Icon>
-                </template>
-            </BaseNavButton>
-        </div>
+  <div class="p-3 w-full md:w-6/12 lg:w-4/12 xl:w-3/12">
+    <div class="bg-white px-6 py-12 rounded-3xl text-center text-gray-400">
+      <img :src="imageUrl" class="mb-4 mx-auto rounded-xl w-full sm:w-9/12" alt="..." width="600" height="600" />
+      <h4 class="font-bold mb-2 text-2xl text-gray-900">{{ name }}</h4>
+      <p class="mb-6 text-sm">
+        {{ breed }} </p>
+      <BaseNavButton :to="`/adopt/${id}`">
+        <span>Adopt Now</span>
+        <template v-slot:append>
+          <Icon name="ri:heart-add-fill" size="24" class="text-yellow-700"></Icon>
+        </template>
+      </BaseNavButton>
     </div>
+  </div>
 </template>
 <style scoped>
-	.adopt-section-card {
-		color: green;
-		font-weight: bold;
-	}
+.adopt-section-card {
+  color: green;
+  font-weight: bold;
+}
 </style>
