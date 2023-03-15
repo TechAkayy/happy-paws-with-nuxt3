e3f7189fe1fb005bcf5081aaf6c9b1235c2439b9
Akayy <techakayy@gmail.com>
2023-03-16 10:48:36 +1100

    Minor fix

--- plugins/devtools.client.ts
+++ plugins/devtools.client.ts
@@ -1,10 +1,10 @@
 import devtools from '@vue/devtools'
 
 export default defineNuxtPlugin((nuxtApp) => {
-	if (process.env.NODE_ENV /* nuxtApp.isDev */ === 'development') {
-		// devtools.connect(/* host, port */)
-		// Expose connect on window's object so that connection with Vue Devtools can be conditionally triggered from within Pinegrow
+  if (process.env.NODE_ENV /* nuxtApp.isDev */ === 'development') {
+    devtools.connect(/* host, port */)
+    // Expose connect on window's object so that connection with Vue Devtools can be conditionally triggered from within Pinegrow
     //@ts-ignore
-		window.devtools = devtools
-	}
+    // window.devtools = devtools
+  }
 })
